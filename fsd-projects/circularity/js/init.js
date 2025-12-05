2b204004d4e753a4e111b8514635a2e11ea3361a
blanch657 <blanch657@wearescpps.org>
2025-12-05 16:58:54 +0000

    Updated Circularity

--- fsd-projects/circularity/js/init.js
+++ fsd-projects/circularity/js/init.js
@@ -93,16 +93,13 @@ var init = function (window) {
             }
             
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
-            if(circle.x === 0){
+            if(circle.x < 0){
                 circle.x = canvas.width;
             }
-            if(circle.x === canvas.width){
-                circle.x = 0;
-            }
-            if(circle.y === 0){
+            if(circle.y < 0){
                 circle.y = canvas.height;
             }
-            if(circle.y === canvas.height){
+            if(circle.y > canvas.height){
                 circle.y = 0;
             }
 
